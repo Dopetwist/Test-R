f14ae8508d3aaf2fa7ff9df000f3c908be9877fd
Jonathan <jonathanafugwobi1@gmail.com>
2025-03-01 08:30:47 +0100

    Update code

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import env from "dotenv";
+
+const app = express();
+const port = 3000;
+
+env.config();
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello Jonathan!");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
